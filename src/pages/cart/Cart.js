6ce05c13fadac08f5d4663a248afb29248306e18
Singh <harjeevs17@gmail.com>
2022-08-23 00:14:34 +0530

    worked on cart page

--- src/pages/cart/Cart.js
+++ src/pages/cart/Cart.js
@@ -1,27 +1,40 @@
+import styled from "@emotion/styled";
+import { Grid, Paper } from "@mui/material";
+import { Container } from "@mui/system";
 import { useContext, useEffect } from "react";
+import { AiFillDelete } from "react-icons/ai";
 import { UserContext } from "../../App";
 import Header from "../../layout/header/Header";
 
-const Cart = () =>{
+const Cart = () => {
     const user = JSON.parse(localStorage.getItem("user"));
-    return(
-        <div className="container">
-            <div className="cartBox">
-                {user.cart.length > 0 ? 
-                    <div>
-                        {user.cart.map((item)=>{
-                            return (
-                                <div>
-                                    <img src={item.data.image} style={{height:"100px"}}/>
-                                    {item.data.title}
-                                </div>
-                            )
-                        })}
-                    </div>
-                    : ""
-                }
-            </div>            
-        </div>
+    const Item = styled(Paper)(({ theme }) => ({
+        textAlign: 'center',
+        height: "170px",
+        boxShadow: "0px 0px 10px rgba(0,0,0,0)",
+        margin: "10px"
+    }));
+    return (
+        <>
+            <Header />
+            <Container sx={{ mt: 10 }} container spacing={5} className="cartBox">
+                {user.cart.length > 0 ? user.cart.map((item, key) => {
+                    return (
+                        <Grid container id={key}>
+                            <Grid sm={4} lg={4}>
+                                <Item><img src={item.data.image} style={{ maxHeight: "100%" }} /></Item>
+                            </Grid>
+                            <Grid sm={4} lg={6}>
+                                <Item style={{ textAlign: "left" }}>{item.data.title}</Item>
+                            </Grid>
+                            <Grid sm={4} lg={2}>
+                                <Item><AiFillDelete /></Item>
+                            </Grid>
+                        </Grid>
+                    )
+                }) : ""}
+            </Container>
+        </>
     )
 }
 export default Cart;
